6d1592a84919c2fd4d04ad60ec1adaa7d3a7ec4f
Yevhen Surzhenko <yevhen.surzhenko1989@gmail.com>
2019-02-14 11:58:15 +0200

    Colors of marker what you can use

--- folium_map_app.py
+++ folium_map_app.py
@@ -1,17 +1,24 @@
 import folium
 
+'''
+from folium import plugins -- icon upgrade 
+The color of the marker. You can use:
+
+            ['red', 'blue', 'green', 'purple', 'orange', 'darkred',
+             'lightred', 'beige', 'darkblue', 'darkgreen', 'cadetblue',
+             'darkpurple', 'white', 'pink', 'lightblue', 'lightgreen',
+             'gray', 'black', 'lightgray']
+'''
 
 # Create Map
-Starobilsk = [49.25,38.91]
+Starobilsk = [49.25, 38.91]
 
 map = folium.Map(location=Starobilsk, zoom_start=8, tiles='Mapbox Bright')
 folium.Marker(location=Starobilsk, popup='Starobilsk', tooltip='Starobilsk',
               icon=folium.Icon(color='red')).add_to(map)
 
-
 # Create LayerControl
 map.add_child(folium.LayerControl())
-
 
 # Save Map like ***.html file
 map.save('Map.html')
