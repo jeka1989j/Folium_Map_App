a5f25d8ca271ba6a307b5cce9168ac4e274dc14a
Yevhen Surzhenko <yevhen.surzhenko1989@gmail.com>
2019-02-19 10:03:46 +0200

    clor_icon func for Markers in our Map

--- folium_map_app.py
+++ folium_map_app.py
@@ -1,12 +1,12 @@
 import folium
 import pandas
 
-# Reading *.xlsx file with data 
+# Reading *.xlsx file with data
 data = pandas.read_excel('datafile/population_2.xlsx')
 lat = list(data['Latitude'])
 lon = list(data['Longitude'])
 city = list(data['region_units'])
-
+population = list(data['population_01.12.18'])
 '''
 from folium import plugins -- icon upgrade 
 The color of the marker. You can use:
@@ -17,13 +17,27 @@ The color of the marker. You can use:
              'gray', 'black', 'lightgray']
 '''
 
+
+# Create func for color Markers Icons
+def color_icon(population):
+    if population < 1000000:
+        return 'red'
+    elif population <= population < 1500000:
+        return 'orange'
+    elif 1500000 <= population < 2000000:
+        return 'green'
+    else:
+        return 'blue'
+
+
 # Create Map
 Starobilsk = [49.25, 38.91]
 
 map = folium.Map(location=Starobilsk, zoom_start=8, tiles='Mapbox Bright')
-for lat, lon, city in zip(lat, lon, city):
-    folium.Marker(location=[lat, lon], popup=city, tooltip=city,
-                  icon=folium.Icon(color='red')).add_to(map)
+for lat, lon, city, popul in zip(lat, lon, city, population):
+    folium.Marker(location=[lat, lon], popup=city,
+                  tooltip=city + ' ' + str(popul),
+                  icon=folium.Icon(color=color_icon(popul))).add_to(map)
 
 # Create LayerControl
 map.add_child(folium.LayerControl())
